567e091212b745fb7ce771d61f2f29c89d642baf
Ensar Emir Erol <ensaremir.erol99@gmail.com>
2023-06-03 16:57:57 +0300

    fix

--- src/main/java/com/dir/music/user_information/clients/AuthServiceFeign.java
+++ src/main/java/com/dir/music/user_information/clients/AuthServiceFeign.java
@@ -1,11 +1,8 @@
 package com.dir.music.user_information.clients;
 
 
-import com.dir.music.user_information.service.auth_service.input.AuthServiceGetClaimsInput;
 import com.dir.music.user_information.service.auth_service.input.AuthServiceRegisterInput;
-import com.dir.music.user_information.service.auth_service.output.AuthServiceGetClaimsOutput;
 import com.dir.music.user_information.service.auth_service.output.AuthServiceRegisterOutput;
-import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +14,6 @@ public interface AuthServiceFeign {
     @PostMapping("/register")
     ResponseEntity<AuthServiceRegisterOutput> register(
             @RequestBody AuthServiceRegisterInput authControllerRegisterInput
-    );
-
-    @PostMapping(path = "/token-validation/get-claims", consumes = "application/json", produces = "application/json")
-    ResponseEntity<AuthServiceGetClaimsOutput> getClaims(
-            @Valid @RequestBody AuthServiceGetClaimsInput tokenValidationControllerInput
     );
 
 
